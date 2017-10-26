b3ee33922a03c8f1465c352bc56363f15d4dfbd3
CHIA-YU-TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-26 18:22:20 +0800

    修改score.blade.php

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -6,7 +6,7 @@
 	</div>
 	<div class="row">
 		<p>
-			學號：{{ Sstudent_no }}
+			學號：{{ $student_no }}
 		</p>
 		<p>
 			姓名：小明
@@ -19,12 +19,12 @@
 				國文：60
 			</p>
 		@endif
-		@if ( is_null($subject) || $subject=='english)
+		@if ( is_null($subject) || $subject=='english')
 			<p>
 				英文：60
 			</p>
 		@endif
-		@if ( is_null($subject) || $subject=='math)
+		@if ( is_null($subject) || $subject=='math')
 			<p>
 				數學：60
 			</p>
