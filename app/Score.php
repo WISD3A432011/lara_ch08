2e97a4439e3be8cdaa1c61c098915290840110d8
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-24 19:03:24 +0800

    練習三：在 Score 當中建立兩個 scope 方法：scopeOrderByTotal() 及scopeOrderBySubject()

--- app/Score.php
+++ app/Score.php
@@ -7,7 +7,19 @@ use \App\Student as StudentEloquent;
 class Score extends Model
 {
     protected $table = 'score';
+
     public function student(){
         return $this->belongsTo(StudentEloquent::class);
     }
+
+    //用以對查詢的資料作排序
+    public function scopeOrderByTotal($query){
+        return $query->orderBy('score.total','DESC');
+    }
+
+    //用以對查詢的資料作排序
+    public function scopeOrderBySubject($query){
+        return $query->orderBy('score.chinese','DESC')->orderBy('score.english','DESC')->orderBy('score.math','DESC');
+    }
+
 }
